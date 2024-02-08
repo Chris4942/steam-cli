204fbdf439814ff2c9a5d4612832d7e791ed772f
Chris West <cjwest4942@gmail.com>
2024-02-08 00:30:47 -0700

    started rebuilding steam cli using clap

---
+++ src/games_service.rs
@@ -0,0 +1,5 @@
+use clap::ArgMatches;
+
+pub fn find_games_in_common(args: &ArgMatches) {
+    println!("in find games in common: {:?}", args);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::{command, value_parser, Arg, Command};
+
+mod games_service;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = command!()
+        .version("0.0")
+        .author("Chris West")
+        .about("Some utility functions for use with steam")
+        .subcommand(
+            Command::new("games-in-common")
+                .alias("gic")
+                .arg(Arg::new("steam_ids")
+                .num_args(1..)
+                .value_parser(value_parser!(u32))
+            )
+        )
+        .get_matches();
+    match matches.subcommand() {
+        Some(("games-in-common", arguments)) => {
+            games_service::find_games_in_common(arguments);
+        }
+        None => {
+            println!("got nothing");
+        }
+        _ => unreachable!(),
+    }
 }
