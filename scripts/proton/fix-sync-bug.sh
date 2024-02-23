db4a2174b18b505f658287b1b1dc64f57e1e1317
Chris West <cjwest4942@gmail.com>
2024-02-23 00:36:14 -0700

    add usage comment

--- scripts/proton/fix-sync-bug.sh
+++ scripts/proton/fix-sync-bug.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# first argument is a path to a zip folder containing the source dll files. The expected internal structure is
+# dlls/
+# - System32/ucrtbase.dll
+# - SysWOW64/ucrtbase.dll
+# the second argument is the path to the game folder, so for Halo on a steam deck it would be:
+# /home/deck/.local/share/Steam/steamapps/compatdata/976730
+# At least that's what it is on my steam deck
+# So if the zip is called windows-dlls.zip in your downloads folder, you would run:
+# ./fix-sync-bug.sh ~/Downloads/windows-dlls.zip /home/deck/.local/share/Steam/steamapps/compatdata/976730
+
 if [ "$#" -lt 2 ]
 then
     echo "two arguments required"
