d340d4974d7838dd0cbe57a88892ce3979bcc048
Chris West <cjwest4942@gmail.com>
2024-03-01 22:36:21 -0700

    add script to install packages that I want to install on any fresh install of SteamOS

---
+++ scripts/dev/install-dev-env-packages.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# installs various packages that I like to include on my steam deck.
+# Whenever updates occur, I have to reinstall these, so I want to keep them all together somewhere.
+
+sudo pacman -S code
