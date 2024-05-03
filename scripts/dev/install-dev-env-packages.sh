cc7bd457c1e0123d8ae1b2202ed66eee05356692
Chris West <cjwest4942@gmail.com>
2024-05-02 19:49:11 -0600

    update install script

--- scripts/dev/install-dev-env-packages.sh
+++ scripts/dev/install-dev-env-packages.sh
@@ -2,4 +2,6 @@
 # installs various packages that I like to include on my steam deck.
 # Whenever updates occur, I have to reinstall these, so I want to keep them all together somewhere.
 
+sudo pacman -Sgq nerd-fonts
+
 sudo pacman -S code neovim tff-nerd-fonts-symbols-common
