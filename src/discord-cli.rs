3ef57e3938d73c2a9f079c9ce66e9e78152c436d
Chris West <cjwest4942@gmail.com>
2024-05-05 22:29:33 -0600

    refactored out some logic

--- src/discord-cli.rs
+++ src/discord-cli.rs
@@ -1,4 +1,6 @@
-use std::env;
+use std::env::{self, VarError};
+use std::fmt::Display;
+use std::num::ParseIntError;
 
 use serenity::all::Ready;
 use serenity::async_trait;
@@ -14,29 +16,34 @@ struct Handler;
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content.starts_with("steam-cli") {
-            let args = msg
-                .content
-                .split(' ')
-                .map(|s| s.to_owned())
-                .collect::<Vec<_>>();
-            match router::run_command(
-                args.into_iter(),
-                Some(
-                    env::var("USER_STEAM_ID")
-                        .expect("TODO, this shouldn't be hardcoded like this here")
-                        .parse::<u64>()
-                        .expect("TODO this shouldn't be hardcoded like this"),
-                ),
-            )
-            .await
-            {
-                Ok(result) => send_message(ctx, msg, result).await,
-                Err(result) => send_message(ctx, msg, result.to_string()).await,
-            }
+            let response = get_steam_cli_response(&ctx, &msg).await;
+            send_message(ctx, msg, response).await;
         }
     }
 
     async fn ready(&self, _ctx: Context, _ready: Ready) {}
+}
+
+async fn get_steam_cli_response<'a>(_ctx: &Context, msg: &Message) -> String {
+    let args = msg
+        .content
+        .split(' ')
+        .map(|s| s.to_owned())
+        .collect::<Vec<_>>();
+    let result = execute_steam_command(args).await;
+
+    match result {
+        Ok(result) => result,
+        Err(result) => result.to_string(),
+    }
+}
+
+async fn execute_steam_command<'a>(args: Vec<String>) -> Result<String, Error<'a>> {
+    Ok(router::run_command(
+        args.into_iter(),
+        Some(env::var("USER_STEAM_ID")?.parse::<u64>()?),
+    )
+    .await?)
 }
 
 async fn send_message(ctx: Context, msg: Message, message: String) {
@@ -68,5 +75,39 @@ async fn main() {
     println!("starting the client");
     if let Err(why) = client.start().await {
         println!("Client error: {why:?}");
+    }
+}
+
+enum Error<'a> {
+    EnvVarMissing(VarError),
+    Parse(ParseIntError),
+    Execution(router::Error<'a>),
+}
+
+impl<'a> Display for Error<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self {
+            Error::EnvVarMissing(err) => write!(f, "EnvVarMissing: {}", err),
+            Error::Parse(err) => write!(f, "Parse: {}", err),
+            Error::Execution(err) => write!(f, "Execution: {}", err),
+        }
+    }
+}
+
+impl<'a> From<VarError> for Error<'a> {
+    fn from(value: VarError) -> Self {
+        Error::EnvVarMissing(value)
+    }
+}
+
+impl<'a> From<ParseIntError> for Error<'a> {
+    fn from(value: ParseIntError) -> Self {
+        Error::Parse(value)
+    }
+}
+
+impl<'a> From<router::Error<'a>> for Error<'a> {
+    fn from(value: router::Error<'a>) -> Self {
+        Error::Execution(value)
     }
 }
