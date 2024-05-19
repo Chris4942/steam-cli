29ac63013e580b1473dd1911112296167ec2e55c
Chris West <64553509+Chris4942@users.noreply.github.com>
2024-05-19 00:00:08 -0600

    Swap default behavior (#4)
    * changed the default behavior so now it will use fuzzy string matching against persona ids by default
    
    * correct help message

--- src/steam/router.rs
+++ src/steam/router.rs
@@ -15,22 +15,21 @@ pub async fn run_command<'a>(
     args: vec::IntoIter<String>,
     user_id: Option<u64>,
 ) -> Result<String, Error> {
-    let by_name_flag = Arg::new("by-name")
-        .help("if present, then steam ids will be interpretted as persona names and resolves against your steam account and your friends steam accounts. This will not work if your friends list contains duplicate persona names")
-        .long("by-name")
-        .short('b')
-        .alias("b")
-        .action(clap::ArgAction::SetTrue);
     let self_flag = Arg::new("self")
         .help("if present, then the calling user will be included as a steam id. In the discord implemenation, then this currently is hard coded to my steam_id")
         .long("self")
         .short('s')
         .alias("s")
         .action(clap::ArgAction::SetTrue);
-    let fuzzy_flag = Arg::new("fuzzy")
-        .help("Use fuzzy matching. Must be used with --by-name (-b).")
-        .long("fuzzy")
-        .short('f')
+    let strict_matching_flag = Arg::new("strict")
+        .help("Use strict string matching against personaname")
+        .long("strict")
+        .short('s')
+        .action(clap::ArgAction::SetTrue);
+    let use_ids_flag = Arg::new("use-ids")
+        .help("Use steamids directly instead of having them looked up dynamically")
+        .long("use-ids")
+        .short('i')
         .action(clap::ArgAction::SetTrue);
     let steam_ids_arg = Arg::new("steam_ids")
         .help("id(s) assoicated with steam account(s), e.g., for accounts 42 and 7: steam-cli gic 7 42")
@@ -51,54 +50,54 @@ pub async fn run_command<'a>(
             Command::new("games-in-common")
                 .about("find the intersection of games owned by provided steam accounts")
                 .alias("gic")
-                .arg(by_name_flag.clone())
-                .arg(fuzzy_flag.clone())
+                .arg(strict_matching_flag.clone())
+                .arg(use_ids_flag.clone())
                 .arg(steam_ids_arg.clone())
                 .arg_required_else_help(true),
         )
         .subcommand(
             Command::new("games-missing-from-group")
-            .about("find the games owned by everyone in the group except for the focused steam account")
-            .alias("gmig")
-            .arg(by_name_flag.clone())
-            .arg(fuzzy_flag.clone())
-            .arg(
-                Arg::new("focus_steam_id")
-                    .help("id associated with the focus steam account")
-                    .num_args(1)
-                    .value_parser(value_parser!(String))
-            )
-            .arg(steam_ids_arg.clone())
-            .arg_required_else_help(true)
+                .about("find the games owned by everyone in the group except for the focused steam account")
+                .alias("gmig")
+                .arg(strict_matching_flag.clone())
+                .arg(use_ids_flag.clone())
+                .arg(
+                    Arg::new("focus_steam_id")
+                        .help("id associated with the focus steam account")
+                        .num_args(1)
+                        .value_parser(value_parser!(String))
+                )
+                .arg(steam_ids_arg.clone())
+                .arg_required_else_help(true)
         )
         .subcommand(
             Command::new("get-available-endpoints")
-            .about("print out all of the available endpoints. You'll probably want to pipe these into another file that you can search through")
+                .about("print out all of the available endpoints. You'll probably want to pipe these into another file that you can search through")
         )
         .subcommand(
             Command::new("get-user-friends-list")
-            .alias("friends")
-            .about("get the friends list of the user")
-            .arg(steam_id_arg.clone())
-            .arg(self_flag.clone())
+                .alias("friends")
+                .about("get the friends list of the user")
+                .arg(steam_id_arg.clone())
+                .arg(self_flag.clone())
         )
         .subcommand(
             Command::new("get-player-summary")
-            .about("get user summary data.")
-            .long_about("get user summary data. Much more data is provided by the steam api than what is exposed by this command. Feel free to submit a PR to update this is you want more")
-            .arg(by_name_flag.clone())
-            .arg(fuzzy_flag.clone())
-            .arg(steam_ids_arg.clone())
-            .arg(self_flag.clone())
-            .arg_required_else_help(true)
+                .about("get user summary data.")
+                .long_about("get user summary data. Much more data is provided by the steam api than what is exposed by this command. Feel free to submit a PR to update this is you want more")
+                .arg(strict_matching_flag.clone())
+                .arg(use_ids_flag.clone())
+                .arg(steam_ids_arg.clone())
+                .arg(self_flag.clone())
+                .arg_required_else_help(true)
         )
         .subcommand(
             Command::new("friends-who-own-game")
-            .arg(
-                Arg::new("gameid")
-                .value_parser(value_parser!(u64))
-            )
-            .arg_required_else_help(true)
+                .arg(
+                    Arg::new("gameid")
+                    .value_parser(value_parser!(u64))
+                )
+                .arg_required_else_help(true)
         )
         .try_get_matches_from(args)?;
     run_subcommand(matches, user_id).await
@@ -255,22 +254,22 @@ async fn get_steam_ids<'a>(
         .get_many::<String>(steam_ids_key)
         .into_iter()
         .flatten();
-    let steam_ids = if arguments.get_flag("by-name") {
+    let steam_ids = if arguments.get_flag("use-ids") {
+        partially_ingested_steam_ids
+            .map(|id| id.parse::<u64>())
+            .collect::<Result<Vec<_>, ParseIntError>>()?
+    } else {
         let user_steam_id = user_steam_id.ok_or(Error::Argument(
             "user_steam_id is required in order to resolve user_steam_ids by persona name"
                 .to_string(),
         ))?;
         let steam_id_strings = partially_ingested_steam_ids.map(|s| s.trim());
-        if arguments.get_flag("fuzzy") {
+        if arguments.get_flag("strict") {
+            service::resolve_usernames_strictly(steam_id_strings, user_steam_id).await?
+        } else {
             service::resolve_usernames_fuzzily(steam_id_strings, user_steam_id, FUZZY_THRESHOLD)
                 .await?
-        } else {
-            service::resolve_usernames_strictly(steam_id_strings, user_steam_id).await?
         }
-    } else {
-        partially_ingested_steam_ids
-            .map(|id| id.parse::<u64>())
-            .collect::<Result<Vec<_>, ParseIntError>>()?
     };
     Ok(steam_ids)
 }
