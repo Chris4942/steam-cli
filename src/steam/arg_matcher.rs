050e6d57d8f2e8c8d2fbce91d02493aec62548e1
Chris West <cjwest4942@gmail.com>
2024-06-05 23:06:32 -0600

    fix extra printing

--- src/steam/arg_matcher.rs
+++ src/steam/arg_matcher.rs
@@ -110,7 +110,7 @@ impl From<ClapError> for Error {
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Error::Matcher(str) => write!(f, "MatcherError: {}", str),
+            Error::Matcher(str) => write!(f, "{str}"),
         }
     }
 }
