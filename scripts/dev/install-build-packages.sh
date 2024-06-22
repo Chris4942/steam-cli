97c4d8e29d426d30402aa110aad7de3dc7934120
Chris West <cjwest4942@gmail.com>
2024-06-21 22:29:43 -0600

    note for self when setting up steam deck

--- scripts/dev/install-build-packages.sh
+++ scripts/dev/install-build-packages.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 sudo pacman -S gcc mingw-w64-gcc openssl pkgconfig linux-api-headers npm
+
+# You'll probably also need to something like this:
+# pacman -S "$package" --overwrite "*" --noconfirm
+# on glibc
