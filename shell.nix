cf16d69ba59a409bc2600793009a88d3536aac49
Chris West <cjwest4942@gmail.com>
2024-09-14 09:24:54 -0400

    add nix-shell that works for compiling to native architecture

---
+++ shell.nix
@@ -0,0 +1,5 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = with pkgs.buildPackages; [ neovim cargo rustc clippy which git openssl_3_3 pkg-config ];
+}
