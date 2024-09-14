acd14c49191ed27a67fc88e5e9d8f666415d8d56
Chris West <cjwest4942@gmail.com>
2024-09-14 09:50:16 -0400

    add shellHook

--- shell.nix
+++ shell.nix
@@ -2,4 +2,8 @@
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = with pkgs.buildPackages; [ neovim cargo rustc clippy which git openssl_3_3 pkg-config ];
+    shellHook = ''
+        alias g=git
+        alias n="nvim ."
+    '';
 }
